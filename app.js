a69c06edd6706e6f3f7d7fd9a3a8eee271ce814c
Rob Serranilla <rob@serranilla.net>
2023-12-05 20:27:06 -0800

    session cookies

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const connect = mongoose.connect(url, {
   useUnifiedTopology: true
 });
 
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
 connect.then(() => console.log('Connected correctly to the server'),
   err => console.log(err)
 );
@@ -35,11 +38,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 //authentication
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+  if (!req.session.user) { //req.signedCookies.user
       const authHeader = req.headers.authorization;
       if (!authHeader) {
           const err = new Error('You are not authenticated!');
@@ -52,7 +65,8 @@ function auth(req, res, next) {
       const user = auth[0];
       const pass = auth[1];
       if (user === 'admin' && pass === 'password') {
-          res.cookie('user', 'admin', {signed: true}); //handles creation of cookies
+          //res.cookie('user', 'admin', {signed: true}); //handles creation of cookies
+          req.session.user = 'admin';
           return next(); // authorized
       } else {
           const err = new Error('You are not authenticated!');
@@ -61,7 +75,7 @@ function auth(req, res, next) {
           return next(err);
       }
   } else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') { //req.signedCookies.user
           return next();
       } else {
           const err = new Error('You are not authenticated!');
